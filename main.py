4a7a47fe7202562aa0450450c781fd387212378c
nkit <nkit237@yandex.ru>
2025-03-01 15:59:18 +0500

    task 2

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('base.html', title='Заготовка')
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
