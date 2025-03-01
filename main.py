9bd0bab813c1ebb4a3728e61e99f52a5383eb1da
nkit <nkit237@yandex.ru>
2025-03-01 16:47:26 +0500

    task 4

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
@@ -36,6 +37,22 @@ def list_prof(list):
         prof_lst=prof,
     )
     return render_template('list_prof.html', **data)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    params = {
+        'title': 'Анкета',
+        'surname': 'Сусанин',
+        'name': 'Иван',
+        'education': '4 класса',
+        'profession': 'экскурсовод',
+        'sex': 'м',
+        'motivation': 'Всегда мечтал застрять на Марсе',
+        'ready': 'готов как никогда'
+    }
+    return render_template('auto_answer.html', **params)
 
 
 if __name__ == '__main__':
