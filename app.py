50607838ff72dfc52d634231de1ae0a798958984
nkit <nkit237@yandex.ru>
2025-03-15 15:29:36 +0500

    task 5

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template
+from data import db_session
+from data.jobs import Jobs
+
+db_session.global_init("db/mars_explorer.db")
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', title='Тренажеры', prof=prof)
+
+
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    prof = [
+        'военные',
+        'охрана',
+        'производители оружия',
+        'испытатели',
+        'мясники',
+        'телохранители',
+        'механики',
+        'инженеры',
+        'дантисты',
+        'повара',
+        'хирурги',
+        'диктаторы',
+    ]
+    data = dict(
+        title='Список профессий',
+        list=list,
+        prof_lst=prof,
+    )
+    return render_template('list_prof.html', **data)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    params = {
+        'title': 'Анкета',
+        'surname': 'Сусанин',
+        'name': 'Иван',
+        'education': '4 класса',
+        'profession': 'экскурсовод',
+        'sex': 'м',
+        'motivation': 'Всегда мечтал застрять на Марсе',
+        'ready': 'готов как никогда'
+    }
+    return render_template('auto_answer.html', **params)
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    return render_template("index.html", jobs=jobs)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
---
+++ data/jobs.py
@@ -0,0 +1,24 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
+    job = sqlalchemy.Column(sqlalchemy.String)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer)
+    collaborators = sqlalchemy.Column(sqlalchemy.String)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                   default=datetime.datetime.now)
+    end_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                 nullable=True)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
+    user = orm.relationship('User')
+
+    def __repr__(self):
+        return f'Jobs(id="{self.id}", job="{self.job}", is_finished="{self.is_finished}")'
